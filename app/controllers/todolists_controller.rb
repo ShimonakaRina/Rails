fa0d23a66378a30f117c88347eb834950140beae
EC2 Default User <ec2-user@ip-172-31-71-5.ec2.internal>
2021-06-08 13:55:12 +0000

    2-7finish

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -33,9 +33,9 @@ def update
 end
 
  private
-  # ストロングパラメータ
+
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a751f0c8afb26d2e9cfed72b3d47952d6d27fca244b9787a4a2fc1a4b7d4c2cd26e96f0b990ff9ef94aa876701203ab649bcf31bc51fac7924340717a8b9392a'
